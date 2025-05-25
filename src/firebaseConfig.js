3c73bca35b755077a28b0c63606ceff7dbb64045
Emmanuel <164503270+Emmanuel-365@users.noreply.github.com>
2025-05-25 14:20:04 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage"; // 👈 Ajouter ceci
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFgmRQKx4M3Gr36ab9S9bxPXknawWb-Sc",
-  authDomain: "planificateur-repas-familiaux.firebaseapp.com",
-  projectId: "planificateur-repas-familiaux",
-  storageBucket: "planificateur-repas-familiaux.appspot.com", // 👈 CORRIGÉ : .app → .com
-  messagingSenderId: "1085846539126",
-  appId: "1:1085846539126:web:6e377351bdf4a186b24857",
-  measurementId: "G-7134J2SZSJ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "", // 👈 CORRIGÉ : .app → .com
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialiser Firebase
