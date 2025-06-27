a5a470ae52fb5cee4fcb5094c4de82a1b04b26ad
Emmanuel <emmanuelscre1@gmail.com>
2025-06-27 08:04:02 +0100

    feat: Update API keys in environment and Chatbot configuration

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,7 +7,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
 // Initialize the Firebase app in the service worker
 
 const firebaseConfig = {
-// insert your firebaseconfig
+  apiKey: "AIzaSyDFgmRQKx4M3Gr36ab9S9bxPXknawWb-Sc",
+  authDomain: "planificateur-repas-familiaux.firebaseapp.com",
+  projectId: "planificateur-repas-familiaux",
+  storageBucket: "planificateur-repas-familiaux.firebasestorage.app",
+  messagingSenderId: "1085846539126",
+  appId: "1:1085846539126:web:6e377351bdf4a186b24857",
+  measurementId: "G-7134J2SZSJ",
 };
 
 firebase.initializeApp(firebaseConfig);
